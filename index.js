82333a94287a553bc395a704f591cfa1cde4581f
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-27 04:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+
+function findMatching(drivers, string){
+  newArray =[];
+  for (let i=0; i<= drivers.legnth; i++){
+    if(drivers[i] === string){
+      newArray.push(drivers[i])
+    }
+  }
+  return newArray
+}
+
+function fuzzyMatch(){
+
+}
+
+
+function matchName(){
+
+}
